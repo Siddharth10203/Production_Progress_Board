0f5e3329233e5327e41e8f1dcd2829a2f9e234ce
Siddharth <1234hunkyhuskies@gmail.com>
2022-07-02 14:50:25 -0700

    double quotes

--- main.py
+++ main.py
@@ -12,8 +12,9 @@ import os
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
 Bootstrap(app)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///production_projects.db'
-# app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///production_projects.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL",
+                                                       "sqlite:///production_projects.db")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
